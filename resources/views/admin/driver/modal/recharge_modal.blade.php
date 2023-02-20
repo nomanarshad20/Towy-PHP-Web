252058db1e4079cda5be62c8b4ba4ad7ebdb26ec
khurram753 <mkhurramilyas@gmail.com>
2023-02-20 12:44:08 +0500

    change modal text

--- resources/views/admin/driver/modal/recharge_modal.blade.php
+++ resources/views/admin/driver/modal/recharge_modal.blade.php
@@ -13,7 +13,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" id="rechargeRecordBtn" class="btn btn-primary">Submiut</button>
+                <button type="button" id="rechargeRecordBtn" class="btn btn-primary">Submit</button>
             </div>
         </div>
     </div>
